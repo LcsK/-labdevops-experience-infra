b50911306b8dfc289dcd579b0f3ee9d27c130541
lcsk <lucasaferrlopes@gmail.com>
2022-03-28 21:40:30 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.14.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "labdevopscloud-6aso-g25"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
